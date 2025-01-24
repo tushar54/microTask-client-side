cc7d609313f7fc87ab3b7feafa9aa868e7fa30a1
tushar54 <tusharahmed7083@gmail.com>
2025-01-25 01:53:26 +0600

    i have added admin home route

--- src/Allroute/Dashboard/Admin/AdminHome.jsx
+++ src/Allroute/Dashboard/Admin/AdminHome.jsx
@@ -1,9 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecure from '../../../AllHooks/useAxiosSecure';
 
 const AdminHome = () => {
+    const axiosSecure=useAxiosSecure()
+    const { data: allinfo, isLoading, error, refetch } = useQuery({
+        queryKey: ['allinfo'],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/allinformation`);
+            return res.data;
+        },
+    });
     return (
         <div>
-            hi i am from admin home
+            
         </div>
     );
 };
